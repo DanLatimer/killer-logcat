fe4129c094da021ff5edcfd2b9545535c65ccd3d
Daniel Latimer <dlatimer@aio-tv.com>
2015-06-14 17:27:29 -0300

    Switch to SQLite database as a data model

--- LogEntryDataStore.js
+++ LogEntryDataStore.js
@@ -1,9 +1,18 @@
 var LogcatEntry = require('./LogcatEntry.js');
 var DataStore = require('./DataStore.js');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
 function LogEntryDataStore() {
-    this.logcatEntries = [];
-    this.tagIndex = new DataStore();
+    db.serialize(function() {
+        db.run("CREATE TABLE LogEntries (" +
+                    "timestamp datetime default current_timestamp," +
+                    "LogLevel VARCHAR(1)," +
+                    "Tag VARCHAR(255)" +
+                    ")");
+    });
+
 }
 
 /**
@@ -12,15 +21,18 @@ function LogEntryDataStore() {
  * @param logEntry
  */
 LogEntryDataStore.prototype.addLogEntry = function(logEntry) {
-    this.logcatEntries.push(logEntry);
 
-    this.tagIndex.addEntry(logEntry.tag, logEntry);
-    
+    var stmt = db.prepare("INSERT INTO LogEntries (LogLevel, Tag) VALUES (?, ?)");
+    stmt.run(logEntry.logLevel, logEntry.tag);
+    stmt.finalize();
+
     this.getUniqueTags();
 };
 
 LogEntryDataStore.prototype.getUniqueTags = function() {
-    console.log(this.tagIndex.toString());
+    db.each("SELECT DISTINCT Tag FROM LogEntries", function(err, row) {
+        console.log(row.Tag);
+    });
 };
 
 module.exports = LogEntryDataStore;
